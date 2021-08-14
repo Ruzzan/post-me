07fe23283e6e1790fb1c108b8a1f29920648d3d7
Ruzzan <rujan.tandukar@gmail.com>
2021-08-14 23:54:54 +0545

    add login page

--- frontend/posts/src/App.js
+++ frontend/posts/src/App.js
@@ -4,14 +4,19 @@ import {Route, Switch} from "react-router-dom";
 import Navbar from './components/Navbar';
 import PostList from './components/List';
 import CreateForm from './components/Create';
+import Login from './components/Auth/Login';
+import {UserProvider} from './context/UserContext';
 
 function App() {
   return (
     <main>
-    <Navbar />
     <Switch>
+    <UserProvider>
+    <Navbar />
     <Route path="/" component={PostList} exact/>
     <Route path="/create" component={CreateForm}/>
+    <Route path="/login" component={Login} />
+    </UserProvider>
     </Switch>
     </main>
   );
