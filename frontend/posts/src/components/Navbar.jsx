750f1beaa4592c9295bbaa0a932622f55a0b97f2
Ruzzan <rujan.tandukar@gmail.com>
2021-08-17 16:00:18 +0545

    add is_auth + navbar conditionals

--- frontend/posts/src/components/Navbar.jsx
+++ frontend/posts/src/components/Navbar.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React,{useContext,useEffect} from 'react';
 import {Link} from "react-router-dom";
-
+import { UserContext } from '../context/UserContext';
 const Navbar = () => {
+    const {user,isAuth} = useContext(UserContext);
+    useEffect(()=>{
+        const checkUser = () => {
+            isAuth();
+        }
+        checkUser();
+    },[])
     return (
         <nav className="navbar navbar-expand-md navbar-light p-1 bg-light">
         <ul className="navbar-nav mx-auto">
         <Link to="/" className="nav-link"> Home </Link>
         <Link to="/create" className="nav-link"> Create </Link>
-        <Link to="/login" className="nav-link"> Login </Link>
+        {user.isAuthenticated === false ? (<Link to="/login" className="nav-link"> Login </Link>) : (<Link to="/logout" className="nav-link"> Logout ({user.user.username}) </Link>)}
         </ul>
         </nav>
     )
