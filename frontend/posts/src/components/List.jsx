76233c79a4853f6887dc204f23803a4e5efa5f36
Ruzzan <rujan.tandukar@gmail.com>
2021-08-17 15:59:38 +0545

    add is_auth + extract token + fetch

--- frontend/posts/src/components/List.jsx
+++ frontend/posts/src/components/List.jsx
@@ -1,5 +1,7 @@
-import React,{useState,useEffect} from 'react';
-import {Post} from './Post';
+import React,{useState,useEffect,useContext} from 'react';
+import {Post} from './Post'; 
+import { UserContext } from '../context/UserContext';
+import {useHistory} from 'react-router-dom';
 
 const axios = require('axios').default;
 
@@ -7,21 +9,35 @@ const PostList = () => {
     const [posts,setPosts] = useState([]);
     const [next,setNext] = useState('');
     const [previous,setPrevious] = useState('');
+
     const [url, setURL] = useState('http://127.0.0.1:8000/api/');
+    
+    const {user,isAuth} = useContext(UserContext)
+
+    let history = useHistory();
+
     useEffect(()=>{
-        const fetchAPI = async () => {
-            const response = await axios.get(url,{
-            headers:{
-                Authorization: 'Token ' + 'ccb2cf08d6912c2254b23660e9cdbafa301fea8f'
-            }
-            });
-            setPosts(response.data.results);
-            setNext(response.data.next);
-            setPrevious(response.data.previous);
-            console.log(response.data.next)
-            console.log(response.data.previous)
+        const checkUserStatus = () => {
+        isAuth();
+        if(!(user.isAuthenticated)) {
+            history.push('/login')
         }
-        fetchAPI();
+        console.log(user.user.token)
+        console.log("authenticated in list")
+        }
+        checkUserStatus()
+    },[])
+
+    useEffect(()=>{
+            const fetchAPI = async () => {
+                const response = await axios.get(url,{
+                headers:{Authorization: 'Token '+user.user.token}
+                });
+                setPosts(response.data.results);
+                setNext(response.data.next);
+                setPrevious(response.data.previous);
+        }
+        fetchAPI();   
     },[url]);
 
     const getPrevPage = () => {
@@ -37,6 +53,7 @@ const PostList = () => {
         <div className="row">
         <div className="col-md-6 mx-auto list-group">
         <h3 className="my-2"> Posts </h3>
+
         {posts.map((post) => {
             return <Post data={post} key={post.id} />
         })}
