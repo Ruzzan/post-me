7b7c7c3671977f774bfdb2a45364bbd90dde57bd
Ruzzan <rujan.tandukar@gmail.com>
2021-08-21 14:20:31 +0545

    add detail

---
+++ frontend/posts/src/components/Detail.jsx
@@ -0,0 +1,60 @@
+import React,{useEffect,useContext,useState} from 'react';
+import {UserContext} from '../context/UserContext';
+import axios from 'axios';
+import {useParams,useHistory} from "react-router-dom";
+
+const PostDetail =  () => {
+    const {postId} = useParams();
+    const history = useHistory();
+
+    const [post,setPost] = useState({});
+    const [loading,setLoading] = useState(false);
+
+    const {user} = useContext(UserContext);
+    let token = user.user.token == null ? "" : user.user.token;
+    const url = `http://127.0.0.1:8000/api/${postId}/`;
+
+    // this will check the user state and fetch data
+
+    useEffect(()=>{
+        const fetchPost = () => {
+          if(!user.isAuthenticated) {
+              history.push('/login')
+          }
+          else {
+            setLoading(true);
+            axios.get(url,{
+                headers:{Authorization:"Token "+token}
+            }).then((data)=>{
+                setPost(data.data);
+                setLoading(false);
+            })
+          }
+        }
+        fetchPost()
+    },[])
+
+    return (
+        <div className="col-md-8 mx-auto my-3">
+        {loading ? 
+        <h6 className="text-center">Loading...</h6>
+        :
+        (
+        <div className="card p-1">
+        <h5>{post.title}</h5>
+        <small>{post.timestamp}</small>
+        <p className="lead"> {post.body} </p>
+        </div>
+        )
+        }
+        </div>
+    );
+}
+
+export default PostDetail;
+
+
+
+
+
+
