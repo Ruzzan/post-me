03af5d0c99573592c232ce670eded65a01d0930a
Ruzzan <rujan.tandukar@gmail.com>
2021-08-21 14:25:30 +0545

    add Link

--- frontend/posts/src/components/Post.jsx
+++ frontend/posts/src/components/Post.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 export const Post = ({data}) => {
     return (
         <div className="my-2 list-group-item list-group-item-action p-1">
-        <h5> {data.title} </h5>
+        <Link to={"/post/"+data.id}><h5> {data.title} </h5></Link>
         <p className="my-1"> {data.body} </p>
         <small> {data.timestamp} </small>
         </div>
