bdd0060c6c51cffbfb19f768afda403ee151d88a
Ruzzan <rujan.tandukar@gmail.com>
2021-09-01 17:43:55 +0545

    add user post list api + url

--- backend/postapi/posts/urls.py
+++ backend/postapi/posts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import PostListAPI,PostRetrieveAPI,UserViewSet
+from .views import PostListAPI,PostRetrieveAPI,UserViewSet,UserPostListAPI
 from rest_framework.routers import SimpleRouter
 
 urlpatterns = [
     path('',PostListAPI.as_view(),name='posts'),
     path('<int:pk>/',PostRetrieveAPI.as_view(),name='detail'),
+    path('user/',UserPostListAPI.as_view(),name='user-post'),
 ]
 
 router = SimpleRouter()
--- backend/postapi/posts/views.py
+++ backend/postapi/posts/views.py
@@ -3,18 +3,18 @@ from .models import Post
 from .serializers import PostSerializer, UserSerializer
 from .pagination import PostPagination
 from .permissions import IsAuthorOrReadOnly
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework import generics
 from rest_framework import viewsets
 from django.contrib.auth import get_user_model
 from dj_rest_auth.views import LoginView
 # Create your views here.
 
-class PostListAPI(ListCreateAPIView):
+class PostListAPI(generics.ListCreateAPIView):
     pagination_class = PostPagination
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
-class PostRetrieveAPI(RetrieveUpdateDestroyAPIView):
+class PostRetrieveAPI(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
@@ -23,3 +23,9 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
     pagination_class = PostPagination
+
+class UserPostListAPI(generics.ListAPIView):
+    serializer_class = PostSerializer
+
+    def get_queryset(self):
+        return Post.objects.filter(author=self.request.user)
